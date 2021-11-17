fd8de1f86846a2635ff90e8c0a45bc3a74883621
jnatonek <jakubnatonek@wp.pl>
2021-11-17 21:25:50 +0100

    quick fix v2.0

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
-const PORT = 3000;
 var PORT = process.env.PORT || 3000;
+var path = require("path");
 const bodyParser = require("body-parser");
 var users = [
     {nick:"111", email:"111@w.pl"},
